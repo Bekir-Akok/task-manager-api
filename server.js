4607be2ed6365be3594bbc053969829f491ddf6f
Bekir Akok <bekir.akok@hotmail.com>
2023-05-09 22:19:53 +0300

    chore: finished

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const router = require("./routes/index");
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT || 4001;
 
+//routes
+app.use("/api", router);
+
 app.use("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "/static/index.html"));
 });
-
-//routes
-app.use("/api", router);
 
 // server listening
 server.listen(port, () => {
