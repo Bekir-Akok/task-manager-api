2607ffba9b20973e93d3e848e6b9d8bbf01e33bb
Bekir Akok <bekir.akok@hotmail.com>
2023-05-09 18:04:47 +0300

    chore: sendfile add to app

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const port = process.env.PORT || API_PORT || 4001;
 //routes
 app.use("/api", router);
 
-//app.use("*", (_req, res) => {
-//res.sendFile(path.join(__dirname, "static/index.html"));
-//});
+app.use("*", (_req, res) => {
+res.sendFile(path.join(__dirname, "/static/static/index.html"));
+});
 
 // server listening
 server.listen(port, () => {
